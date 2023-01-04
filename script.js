5a626967bbceaa638887a9540523a93c0a6301c9
insertSoul <j.clarke213@gmail.com>
2023-01-04 20:08:18 +0000

    Create a function to do all basic maths operators

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ const multiply = function(num1, num2) {
 const divide = function(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+    if (operator == 'add') {
+        return add(num1, num2);
+    } else if (operator == 'subtract') {
+        return subtract(num1, num2);
+    } else if (operator == 'multiply') {
+        return multiply(num1, num2);
+    } else if (operator == 'divide') {
+        return divide(num1, num2);
+    }
+}
