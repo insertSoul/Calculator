ba1281deadb5832a00dfc5c8622c9ee62d21720c
insertSoul <j.clarke213@gmail.com>
2023-01-06 23:40:00 +0000

    Add single digits to display

--- script.js
+++ script.js
@@ -25,3 +25,48 @@ function operate(num1, num2, operator) {
         return divide(num1, num2);
     }
 }
+
+function populateDisplay(output) {
+    display.textContent = output
+
+}
+
+const display = document.querySelector('.display')
+const button1 = document.querySelector('#button1')
+const button2 = document.querySelector('#button2')
+const button3 = document.querySelector('#button3')
+const button4 = document.querySelector('#button4')
+const button5 = document.querySelector('#button5')
+const button6 = document.querySelector('#button6')
+const button7 = document.querySelector('#button7')
+const button8 = document.querySelector('#button8')
+const button9 = document.querySelector('#button9')
+
+
+button1.addEventListener('click', () => {
+    populateDisplay('1');
+});
+button2.addEventListener('click', () => {
+    populateDisplay('2')
+})
+button3.addEventListener('click', () => {
+    populateDisplay('3')
+})
+button4.addEventListener('click', () => {
+    populateDisplay('4')
+})
+button5.addEventListener('click', () => {
+    populateDisplay('5')
+})
+button6.addEventListener('click', () => {
+    populateDisplay('6')
+})
+button7.addEventListener('click', () => {
+    populateDisplay('7')
+})
+button8.addEventListener('click', () => {
+    populateDisplay('8')
+})
+button9.addEventListener('click', () => {
+    populateDisplay('9')
+})
